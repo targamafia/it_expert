d37ffdb056633833ab17d32a27ff94e7c7435de8
Fernando Melgar <fernandom.melgar@gmail.com>
2022-11-03 10:46:08 -0600

    Add loading indicator on login

--- lib/ui/user/login/login_controller.dart
+++ lib/ui/user/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:it_expert/core/user/application/usecase/login_usecase.dart';
 import 'package:it_expert/core/user/domain/dto/login_failed_dto.dart';
+import 'package:it_expert/core/utils/status.dart';
 
 import '../../../core/user/dependency_injection.dart';
 
@@ -9,12 +10,15 @@ class LoginController extends GetxController {
   var password = "".obs;
   var loading = false.obs;
 
+  var status = Status.NOT_STARTED.obs;
+
   var loginSuccess = false;
   var errorMessage = "";
 
   final LoginUseCase _loginUseCase = constructLoginUseCase();
 
   login() async {
+    status(Status.LOADING);
     loading(true);
     loginSuccess = false;
     errorMessage = "";
@@ -22,11 +26,13 @@ class LoginController extends GetxController {
     var result = await _loginUseCase.call(email.value, password.value);
     if (result.isSuccess) {
       loginSuccess = true;
+      status(Status.SUCCESS);
     }
 
     if (result.isFailure && result.exceptionOrNull() is LoginFailedException) {
       errorMessage =
           (result.exceptionOrNull() as LoginFailedException).errorMessage;
+      status(Status.ERROR);
     }
 
     loading(false);
